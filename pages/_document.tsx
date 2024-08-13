8dbe2254c17896b835f9ee291104f7e29a5810bd
drakkola <debbichi1997@gmail.com>
2024-08-13 18:04:57 +0100

    meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,10 @@ export default function Document() {
           name="apple-mobile-web-app-title"
           content="Real-Time Object Detection"
         />
-        <meta name="description" content="real time object detection using yolo demo" />
+        <meta
+          name="description"
+          content="real time object detection using yolo demo"
+        />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/icons/browserconfig.xml" />
@@ -20,12 +23,15 @@ export default function Document() {
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
 
-        <link rel="apple-touch-icon" href="/icon.png" />
+        <link rel="apple-touch-icon" href="/neuron-q.png" />
 
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/icon.png" />
-        
-        <meta name="google-site-verification" content="kPFEI7Zrf_ZNTei2hRsBk-luHRtoO6a9AvndfpekAc8" />
+        <link rel="icon" href="/neuron-q.png" />
+
+        <meta
+          name="google-site-verification"
+          content="kPFEI7Zrf_ZNTei2hRsBk-luHRtoO6a9AvndfpekAc8"
+        />
       </Head>
       <body>
         <Main />
